95bdb8c7091bd01fb6281443c82148a585462790
Chinwendu joy Enyidiegwu <chinwe640@gmail.com>
2023-02-07 00:26:35 +0100

    composables

--- src/composables/color-manager.js
+++ src/composables/color-manager.js
@@ -1,15 +1,19 @@
-import { ref, reactive } from '@vue/reactivity'
+import { ref } from '@vue/reactivity'
 
-export default function ColorManager() {
+export default function colorManager() {
 	const colors = ['green', 'red', 'blue', 'purple']
-	const message = ref('Pick a color...')
+	let message = ref('Pick a color...')
+
 	const matchColor = (value) => {
-		const randomIndex = Math.floor(Math.random() * colors.length)
-		if (colors[randomIndex] === value) {
-			message.value = `You win... [answer: ${colors[randomIndex]}]`
+		// do a random color based on the array index;
+		const randomNumber = Math.floor(Math.random() * 3) + 1 //between 1 - 4
+
+		if (colors[randomNumber] === value) {
+			message.value = `You win... [answer: ${colors[randomNumber]}]`
 			return
 		}
-		message.value = `You lose [answer: ${colors[randomIndex]}]`
+
+		message.value = `You loose [answer: ${colors[randomNumber]}]`
 	}
 	return { colors, message, matchColor }
 }
